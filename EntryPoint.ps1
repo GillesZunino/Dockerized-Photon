311d7e3f7a8f1cd73d8a11010c6b0c9572e99423
Gilles Zunino <GillesZunino@users.noreply.github.com>
2020-06-06 19:34:45 -0700

    Only resolve 'myip.opendns.com' when PHOTON_ENDPOINT is not specified

--- EntryPoint.ps1
+++ EntryPoint.ps1
@@ -15,6 +15,16 @@
 [string] $PhotonSocketServerDirectoryRelativePath = ".\deploy\bin_Win64"
 [string] $PhotonSocketServerExecutableName = "PhotonSocketServer"
 
+# CloudFlare nameserver
+[string] $NameServer = "1.1.1.1"
+
+# Number of attempts to resolve the DNS endpoint of the container
+[int] $DnsRetries = 10
+
+# Current Azure Container Instance TTL on A records is 14
+[int] $DnsWaitInSeconds = 15
+
+
 # Derived constants - Ready to use paths to various executable and files 
 [string] $PhotonSocketServerExecutableRelativePath = Join-Path -Path $PhotonSocketServerDirectoryRelativePath -ChildPath $PhotonSocketServerExecutableName
 
@@ -23,14 +33,14 @@
 . .\Configure-LoadBalancingApp.ps1
 
 
-function Resolve-PhotonEndpoint([string] $photonEndpoint, [string] $nameServer, [int] $numberOfRetries, [int] $secondsBetweenRetries)
+function Resolve-PhotonEndpoint([string] $endpoint, [string] $nameServer, [int] $numberOfRetries, [int] $secondsBetweenRetries)
 {
     [int] $retries = $numberOfRetries
     [int] $waitInSeconds = $secondsBetweenRetries
 
     do {
         try {
-            Write-Host "Resolving '$endpoint'"
+            Write-Host "Resolve-DnsName -Name $endpoint -Type A -Server $nameServer"
             $dnsEntry = Resolve-DnsName -Name $endpoint -Type A -Server $nameServer
             return $dnsEntry.IPAddress
         }
@@ -62,40 +72,34 @@ function Get-PhotonPublicIp([string] $photonEndpoint)
         return $photonEndpoint
     }
 
-    # CloudFlare nameserver
-    [string] $cloudFlareNameserver = "1.1.1.1"
+    # Special case for PHOTON_ENDPOINT null or empty - Lookup 'myip.opendns.com' against 'resolver1.opendns.com'
+    if ([string]::IsNullOrEmpty($photonEndpoint)) {
+        Write-Host "Resolving 'myip.opendns.com' using nameserver 'resolver1.opendns.com'" 
+        [string] $inferedIp = Resolve-PhotonEndpoint "myip.opendns.com" "resolver1.opendns.com" $DnsRetries $DnsWaitInSeconds
 
-    # Number of attempts to resolve the DNS endpoint of the container
-    [int] $retries = 10
-
-    # Current Azure Container Instance TTL on A records is 14
-    [int] $waitInSeconds = 15
-    
-    # Resolve 'myip.opendns.com' against 'resolver1.opendns.com' will return our public IP
-    Write-Host "Resolving 'myip.opendns.com' using nameserver 'resolver1.opendns.com'" 
-    [string] $inferedIp = Resolve-PhotonEndpoint "myip.opendns.com" "resolver1.opendns.com" $retries $waitInSeconds
-    if (![string]::IsNullOrEmpty($inferedIp)) {
-        Write-Host "OpenDNS resolved my IP to '$inferedIp'"
-        return $inferedIp
-    } else {
-        # When re-creating a container shortly after deleting it, the DNS name may still point to the previous IP
-        # We keep on resolving the IP and waiting for TTL to expire until we get to a stable IP
-        Write-Host "Waiting for public IP to stabilize"
-        [string] $ip1 = $null
-        [string] $ip2 = $null
-        do {
-            $ip1 = $null
-            $ip2 = $null
-
-            $ip1 = Resolve-PhotonEndpoint $photonEndpoint $cloudFlareNameserver $retries $waitInSeconds
-            Start-Sleep $waitInSeconds
-            $ip2 = Resolve-PhotonEndpoint $photonEndpoint $cloudFlareNameserver $retries $waitInSeconds
-
-            Write-Host "Resolved '$photonEndpoint' to '$ip1' and '$ip2'"
-        } while ([string]::IsNullOrEmpty($ip1) -or [string]::IsNullOrEmpty($ip2) -or ($ip1 -ne $ip2))
-
-        return $ip1
+        if (![string]::IsNullOrEmpty($inferedIp)) {
+            Write-Host "OpenDNS resolved my IP to '$inferedIp'"
+            return $inferedIp
+        }
     }
+
+    # When re-creating a container shortly after deleting it in Azure Container Instances, the DNS name may still point to the previous IP
+    # We keep on resolving the IP and waiting for TTL to expire until we get to a stable IP
+    Write-Host "Resolving '$photonEndpoint' using '$NameServer' and waiting for IP addresses to stabilize"
+    [string] $ip1 = $null
+    [string] $ip2 = $null
+    do {
+        $ip1 = $null
+        $ip2 = $null
+
+        $ip1 = Resolve-PhotonEndpoint $photonEndpoint $NameServer $DnsRetries $DnsWaitInSeconds
+        Start-Sleep $DnsWaitInSeconds
+        $ip2 = Resolve-PhotonEndpoint $photonEndpoint $NameServer $DnsRetries $DnsWaitInSeconds
+
+        Write-Host "Resolved '$photonEndpoint' to '$ip1' and '$ip2'"
+    } while ([string]::IsNullOrEmpty($ip1) -or [string]::IsNullOrEmpty($ip2) -or ($ip1 -ne $ip2))
+
+    return $ip1
 }
 
 function Register-PerformanceCounters([bool] $registerPerformanceCounters)
@@ -115,39 +119,42 @@ function Register-PerformanceCounters([bool] $registerPerformanceCounters)
 ## ----------------------------------------------------------------------------------------------------------------------------
 
 [string] $endpoint = $Env:PHOTON_ENDPOINT
-Write-Host "DNS Name: $endpoint"
+Write-Host "PHOTON_ENDPOINT: $endpoint"
 
 [string] $loadBalancerPublicIP = Get-PhotonPublicIp $endpoint
-Write-Host "Public IP: $loadBalancerPublicIP"
+Write-Host "GameServer client accessible IP: $loadBalancerPublicIP"
+if (-not [string]::IsNullOrEmpty($loadBalancerPublicIP)) {
+    Write-Host "Configuring LoadBalancing application"
+    Configure-LoadBalancingApp $loadBalancerPublicIP $enablePerformanceCounters
 
-Write-Host "Configuring LoadBalancing application"
-Configure-LoadBalancingApp $loadBalancerPublicIP $enablePerformanceCounters
+    # Register Windows performance counters
+    Register-PerformanceCounters $enablePerformanceCounters
 
-# Register Windows performance counters
-Register-PerformanceCounters $enablePerformanceCounters
+    # Start PhotonSocketServer
+    Write-Host "Starting PhotonSocketServer"
+    [System.Diagnostics.ProcessStartInfo] $processInfo = New-Object System.Diagnostics.ProcessStartInfo
+    $processInfo.WorkingDirectory = $PhotonSocketServerDirectoryRelativePath
+    $processInfo.FileName = $PhotonSocketServerExecutableRelativePath
+    $processInfo.Arguments = "/run LoadBalancing"
+    $processInfo.RedirectStandardInput = $false
+    $processInfo.RedirectStandardOutput = $false
+    $processInfo.RedirectStandardError = $false
+    $processInfo.UseShellExecute = $false
+    [System.Diagnostics.Process] $photonProcess = New-Object System.Diagnostics.Process
+    $photonProcess.StartInfo = $processInfo
+    $photonProcess.Start() | Out-Null
 
-# Start PhotonSocketServer
-Write-Host "Starting PhotonSocketServer"
-[System.Diagnostics.ProcessStartInfo] $processInfo = New-Object System.Diagnostics.ProcessStartInfo
-$processInfo.WorkingDirectory = $PhotonSocketServerDirectoryRelativePath
-$processInfo.FileName = $PhotonSocketServerExecutableRelativePath
-$processInfo.Arguments = "/run LoadBalancing"
-$processInfo.RedirectStandardInput = $false
-$processInfo.RedirectStandardOutput = $false
-$processInfo.RedirectStandardError = $false
-$processInfo.UseShellExecute = $false
-[System.Diagnostics.Process] $photonProcess = New-Object System.Diagnostics.Process
-$photonProcess.StartInfo = $processInfo
-$photonProcess.Start() | Out-Null
+    [int] $photonProcessId = $photonProcess.Id
+    Write-Host "PhotonSocketServer has PID '$photonProcessId'"
 
-[int] $photonProcessId = $photonProcess.Id
-Write-Host "PhotonSocketServer has PID '$photonProcessId'"
+    # Become a loving parent of PhotonSocketServer.exe
+    Write-Host "Waiting for PhotonSocketServer to exit"
+    $photonProcess.WaitForExit()
 
-# Become a loving parent of PhotonSocketServer.exe
-Write-Host "Waiting for PhotonSocketServer to exit"
-$photonProcess.WaitForExit()
-
-# Throws when it exits so we can indicate the container terminated
-[int] $photonExitCode = $photonProcess.ExitCode
-Write-Host "PhotonSocketServer has exited with code '$photonExitCode'"
-throw "PhotonSocketServer exited - $photonExitCode"
+    # Throws when it exits so we can indicate the container terminated
+    [int] $photonExitCode = $photonProcess.ExitCode
+    Write-Host "PhotonSocketServer has exited with code '$photonExitCode'"
+    throw "PhotonSocketServer exited - $photonExitCode"
+} else {
+    throw "Could not find GameServer client accessible IP"
+}
